a1d0550894fd88c3b3d4b6a04c3ccdb1f7ebc093
almaghrabima <almaghrabima@gmail.com>
2025-12-01 11:55:34 +0000

    Update build_and_push.sh to use sudo for Docker commands

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,13 +5,13 @@ LOCAL_IMAGE="advance-deeplearning-setup"
 TAG="torch2.8-cuda12.8"
 REMOTE_IMAGE="almamoha/advance-deeplearning:${TAG}"
 
-echo "🐳 Building Docker image: ${LOCAL_IMAGE}"
-docker build -t "${LOCAL_IMAGE}" .
+echo "🐳 Building docker image: ${LOCAL_IMAGE}"
+sudo sudo docker build -t "${LOCAL_IMAGE}" .
 
 echo "🏷️ Tagging image as: ${REMOTE_IMAGE}"
-docker tag "${LOCAL_IMAGE}" "${REMOTE_IMAGE}"
+sudo docker tag "${LOCAL_IMAGE}" "${REMOTE_IMAGE}"
 
 echo "⬆️ Pushing image: ${REMOTE_IMAGE}"
-docker push "${REMOTE_IMAGE}"
+sudo docker push "${REMOTE_IMAGE}"
 
 echo "✅ Done. Image pushed: ${REMOTE_IMAGE}"
